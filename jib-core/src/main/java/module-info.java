16b927327a1f3d5963d051721fc90743ef8b4c68
Chanseok Oh <chanseok@google.com>
2019-01-16 13:45:31 -0500

    Unnamed Mockito 2.23.17

--- jib-core/src/main/java/module-info.java
+++ jib-core/src/main/java/module-info.java
@@ -1,5 +1,32 @@
+open // opens everything
 module com.google.cloud.tools.jib {
+  exports com.google.cloud.tools.jib;
   exports com.google.cloud.tools.jib.api;
+  exports com.google.cloud.tools.jib.async;
+  exports com.google.cloud.tools.jib.blob;
+  exports com.google.cloud.tools.jib.builder;
+  exports com.google.cloud.tools.jib.builder.steps;
+  exports com.google.cloud.tools.jib.cache;
+  exports com.google.cloud.tools.jib.configuration;
+  exports com.google.cloud.tools.jib.configuration.credentials;
+  exports com.google.cloud.tools.jib.docker;
+  exports com.google.cloud.tools.jib.docker.json;
+  exports com.google.cloud.tools.jib.event;
+  exports com.google.cloud.tools.jib.event.events;
+  exports com.google.cloud.tools.jib.event.progress;
+  exports com.google.cloud.tools.jib.filesystem;
+  exports com.google.cloud.tools.jib.frontend;
+  exports com.google.cloud.tools.jib.global;
+  exports com.google.cloud.tools.jib.hash;
+  exports com.google.cloud.tools.jib.http;
+  exports com.google.cloud.tools.jib.image;
+  exports com.google.cloud.tools.jib.image.json;
+  exports com.google.cloud.tools.jib.json;
+  exports com.google.cloud.tools.jib.registry;
+  exports com.google.cloud.tools.jib.registry.credentials;
+  exports com.google.cloud.tools.jib.registry.credentials.json;
+  exports com.google.cloud.tools.jib.registry.json;
+  exports com.google.cloud.tools.jib.tar;
 
   requires java.logging;
   requires com.fasterxml.jackson.databind;
